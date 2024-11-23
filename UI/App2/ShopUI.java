be95293a02e4502d5af50c7a5f55f1f247e9ed91
Chocon2911 <131384762+Chocon2911@users.noreply.github.com>
2024-11-23 15:09:02 +0700

    24-11-23 15h08 LeXuanHuy

---
+++ UI/App2/ShopUI.java
@@ -0,0 +1,154 @@
+package UI.App2;
+
+import Obj.Main.Shop;
+import Util.GuiUtil;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ShopUI extends GuiUtil
+{
+    //==========================================Variable==========================================
+    private Shop shop;
+
+    //========================================Constructor=========================================
+    public ShopUI()
+    {
+        this.shop = null;
+        this.displayMain();
+    }
+
+    public ShopUI(Shop shop)
+    {
+        this.shop = shop;
+        this.displayMain();
+    }
+
+    //==========================================Main UI===========================================
+    private void displayMain()
+    {
+        // Frame
+        JFrame frame = new JFrame("App2.Shop.Main");
+        frame.setSize(frameWidth, frameHeight);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Main Panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        // Title Label
+        JLabel titleLabel = new JLabel("Shop");
+        this.setAlignmentCenter(titleLabel);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, titleSize));
+
+        // Information Button
+        JButton infoButton = createButton("Information", bigButtonWidth, bigButtonHeight);
+        this.setAlignmentCenter(infoButton);
+        infoButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                frame.dispose();
+                displayInfo();
+            }
+        });
+
+        // CreateManager Button
+        JButton createManagerButton = createButton("Create Manager", bigButtonWidth, bigButtonHeight);
+        this.setAlignmentCenter(createManagerButton);
+        createManagerButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                frame.dispose();
+                displayCreateManager();
+            }
+        });
+
+        // ChangeCheckInCode Button
+        JButton changeCheckInCodeButton = createButton("Change Check In Code", bigButtonWidth, bigButtonHeight);
+        this.setAlignmentCenter(changeCheckInCodeButton);
+        changeCheckInCodeButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                frame.dispose();
+                displayChangeCheckInCode();
+            }
+        });
+
+        // Quit Button
+        JButton quitButton = createButton("Quit", bigButtonWidth, bigButtonHeight);
+        this.setAlignmentCenter(quitButton);
+        quitButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                frame.dispose();
+                displayQuit();
+            }
+        });
+
+        // Display
+        panel.add(Box.createVerticalGlue());
+        panel.add(titleLabel);
+        panel.add(Box.createVerticalStrut(this.verticalStrut));
+        panel.add(infoButton);
+        panel.add(Box.createVerticalStrut(this.verticalStrut));
+        panel.add(createManagerButton);
+        panel.add(Box.createVerticalStrut(this.verticalStrut));
+        panel.add(changeCheckInCodeButton);
+        panel.add(Box.createVerticalStrut(this.verticalStrut));
+        panel.add(quitButton);
+        panel.add(Box.createVerticalGlue());
+        
+        frame.add(panel);
+        frame.setVisible(true);
+        
+    }
+
+    //=======================================Information UI=======================================
+    private void displayInfo()
+    {
+        // ===Frame===
+        JFrame frame = new JFrame("App2.Shop.Main.Information");
+        frame.setSize(frameWidth, frameHeight);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    //=====================================Create Manager UI======================================
+    private void displayCreateManager()
+    {
+
+    }
+
+    //===================================Change CheckIn Code UI===================================
+    private void displayChangeCheckInCode()
+    {
+
+    }
+
+    //==========================================Quit UI===========================================
+    private void displayQuit()
+    {
+        
+    }
+
+    //============================================Test============================================
+    public static void main(String[] args) 
+    {
+        new ShopUI();
+    }
+}
--- Util/GuiUtil.java
+++ Util/GuiUtil.java
@@ -5,6 +5,30 @@ import javax.swing.*;
 
 public abstract class GuiUtil
 {
+    //==========================================Variable==========================================
+    protected int frameWidth = 500;
+    protected int frameHeight = 600;
+
+    protected int horizontalStrut = 20;
+    protected int verticalStrut = 20;
+
+    protected int normalLabelWidth = 100;
+    protected int normalLabelHeight = 20;
+
+    protected int panelFieldWidth = 300;
+    protected int panelFieldHeight = 25;
+
+    protected int bigButtonWidth = 200;
+    protected int bigButtonHeight = 50;
+
+    protected int smallButtonWidth = 100;
+    protected int smallButtonHeight = 25;
+
+    protected int textFieldAmount = 20;
+
+    protected int titleSize = 50;
+
+    //===========================================Method===========================================
     protected void setFixedSize(JComponent component, int width, int height)
     {
         component.setPreferredSize(new Dimension(width, height));
